c1ada8ba40ad66a0f6c658067e12f120a4d0e8b0
Husam Alshehadat <halshehadat@proton.me>
2024-11-28 04:40:54 -0800

    fix the circular imports

--- backend/src/modules/roadmaps/__init__.py
+++ backend/src/modules/roadmaps/__init__.py
@@ -1,25 +1,4 @@
-from src.modules.roadmaps.api.schemas import (
-    NodeBase,
-    NodeCreate,
-    NodeResponse,
-    NodeUpdate,
-    RoadmapBase,
-    RoadmapCreate,
-    RoadmapResponse,
-    RoadmapUpdate,
-)
 from src.modules.roadmaps.domain.models import Node, Roadmap
 
 
-__all__ = [
-    "Node",
-    "NodeBase",
-    "NodeCreate",
-    "NodeResponse",
-    "NodeUpdate",
-    "Roadmap",
-    "RoadmapBase",
-    "RoadmapCreate",
-    "RoadmapResponse",
-    "RoadmapUpdate",
-]
+__all__ = ["Node", "Roadmap"]
