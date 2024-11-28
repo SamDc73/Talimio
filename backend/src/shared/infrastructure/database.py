af9daf3dfc2fa1c3503f82e5c9700b725297b1a3
Husam Alshehadat <halshehadat@proton.me>
2024-11-28 04:36:56 -0800

    cleaned up the db side

--- backend/src/shared/infrastructure/database.py
+++ backend/src/shared/infrastructure/database.py
@@ -1,27 +1,19 @@
-import os
 from collections.abc import AsyncGenerator
 
 from dotenv import load_dotenv
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-from src.config.postgres import pg_settings
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import declarative_base
+from src.config.database import db_settings
 
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Get the DATABASE_URL from environment variables
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    msg = "DATABASE_URL is not set in the environment variables"
-    raise ValueError(msg)
-
 # Create an asynchronous engine with the asyncpg driver
-engine = create_async_engine(pg_settings.DATABASE_URL, echo=True)
+engine = create_async_engine(db_settings.DATABASE_URL, echo=True)
 
 # Create an asynchronous sessionmaker
-AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+AsyncSessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 # Create a declarative base for ORM models
 Base = declarative_base()
