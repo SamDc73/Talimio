f96b77d3ed9459dc0fb4b4900eeda71672d1d3dc
Husam Alshehadat <halshehadat@proton.me>
2025-04-17 14:02:28 -0700

    feat: support viewing specific roadmap by URL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 
-import './index.css'
-import App from './App'
+import "./index.css";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      {" "}
+      {/* Wrap App with BrowserRouter */}
+      <App />
+    </BrowserRouter>
   </StrictMode>
-)
+);
