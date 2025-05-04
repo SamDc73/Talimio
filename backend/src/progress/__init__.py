921fd178de109debd36b5d6d0bb94f3ed9c223a5
SamDc73 <halshehadat@proton.me>
2025-05-04 11:09:17 -0700

    remove users and progress endpoints

--- backend/src/progress/__init__.py
+++
@@ -1,5 +0,0 @@
-from src.progress.models import Progress
-from src.progress.schemas import ProgressCreate, ProgressResponse
-
-
-__all__ = ["Progress", "ProgressCreate", "ProgressResponse"]
--- backend/src/users/__init__.py
+++
@@ -1,5 +0,0 @@
-from src.users.models import User
-from src.users.schemas import UserCreate, UserResponse, UserUpdate
-
-
-__all__ = ["User", "UserCreate", "UserResponse", "UserUpdate"]
